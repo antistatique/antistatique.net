1688a45b57b3713b3ff7edf5e671d1b04fca149a
Toni Fisler <mail@tonifisler.ch>
2015-03-06 10:39:41 +0100

    add current assets and styleguide to project!

---
+++ drupal/sites/all/themes/antistatique/assets/README.md
@@ -0,0 +1,126 @@
+# Antistatique
+
+```none_example
+
+<div class="row">
+  <div class="col-md-12">
+    <a href="pages/index.html" class="btn btn-default pull-right">Go to Example Pages</a>
+  </div>
+  <div class="col-md-12">
+    <p class="text-muted">Text and fonts</p>
+  </div>
+  <div class="col-md-6">
+    <h1>H1. Dagny Extra Light</h1>
+    <h2>H2. Dagny Bold</h2>
+    <h3>H3. Dagny Extra Light</h3>
+    <h4>H4. Dagny Bold</h4>
+    <h5>H5. Dagny Extra Light</h5>
+    <h6>H6. Dagny Bold</h6>
+    <hr>
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, minus, possimus minima quibusdam nihil dolorem. Itaque, exercitationem, perferendis.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing <a href="#">Link</a>. Earum perferendis incidunt eaque itaque, sed quos, ullam obcaecati perspiciatis deserunt nihil, unde iste vitae inventore eum. Delectus error veniam libero hic.</p>
+    <p>Lorem ipsum <strong>Strong</strong> sit amet, consectetur adipisicing elit. Accusantium doloribus totam debitis, illo architecto eos. Repellat earum id tempora optio <em>Italic</em> quas atque laborum odit ullam necessitatibus animi, eligendi deserunt!</p>
+    <p><small class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</small></p>
+    <ul class="list-dash">
+      <li><strong>Gras</strong></li>
+      <li><a href="#">Liens</a></li>
+      <li><strong><a href="#">Liens Gras</a></strong></li>
+    </ul>
+    <ul>
+      <li>List item 1</li>
+      <li>List item 2
+        <ul>
+          <li>List item 3</li>
+          <li>List item 4</li>
+        </ul>
+      </li>
+    </ul>
+    <ol>
+      <li>List item 1</li>
+      <li>List item 2
+        <ol>
+          <li>List item 3</li>
+          <li>List item 4</li>
+        </ol>
+      </li>
+    </ol>
+  </div>
+  <div class="col-md-6 inverse">
+    <h1>H1. Dagny Extra Light</h1>
+    <h2>H2. Dagny Bold</h2>
+    <h3>H3. Dagny Extra Light</h3>
+    <h4>H4. Dagny Bold</h4>
+    <h5>H5. Dagny Extra Light</h5>
+    <h6>H6. Dagny Bold</h6>
+    <hr>
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, minus, possimus minima quibusdam nihil dolorem. Itaque, exercitationem, perferendis.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing <a href="#">Link</a>. Earum perferendis incidunt eaque itaque, sed quos, ullam obcaecati perspiciatis deserunt nihil, unde iste vitae inventore eum. Delectus error veniam libero hic.</p>
+    <p>Lorem ipsum <strong>Strong</strong> sit amet, consectetur adipisicing elit. Accusantium doloribus totam debitis, illo architecto eos. Repellat earum id tempora optio <em>Italic</em> quas atque laborum odit ullam necessitatibus animi, eligendi deserunt!</p>
+    <p><small class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</small></p>
+    <ul class="list-dash">
+      <li><strong>Gras</strong></li>
+      <li><a href="#">Liens</a></li>
+      <li><strong><a href="#">Liens Gras</a></strong></li>
+    </ul>
+    <ul>
+      <li>List item 1</li>
+      <li>List item 2
+        <ul>
+          <li>List item 3</li>
+          <li>List item 4</li>
+        </ul>
+      </li>
+    </ul>
+    <ol>
+      <li>List item 1</li>
+      <li>List item 2
+        <ol>
+          <li>List item 3</li>
+          <li>List item 4</li>
+        </ol>
+      </li>
+    </ol>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-6">
+    <h3 class="text-muted">Lists</h3>
+    <ul>
+      <li>Lorem</li>
+      <li>Ipsum</li>
+      <li>Dolor</li>
+      <li>Sit amet</li>
+    </ul>
+    <div class="spacer"></div>
+    <ol>
+      <li>Lorem</li>
+      <li>Ipsum</li>
+      <li>Dolor</li>
+      <li>Sit amet</li>
+    </ol>
+  </div>
+  <div class="col-md-6">
+    <h3 class="text-muted">Components</h3>
+    <form role="form">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+      </div>
+    </form>
+    <p><button class="btn btn-primary">Primary</button></p>
+    <p><button class="btn btn-primary btn-lg">Primary Large</button></p>
+    <p><button class="btn btn-success">Success</button></p>
+    <p><button class="btn btn-danger">Danger</button></p>
+    ...
+  </div>
+</div>
+```
+
+**Typekit code:**
+
+```js
+<script src="//use.typekit.net/lfl3jyz.js"></script>
+<script>try{Typekit.load();}catch(e){}</script>
+```
---
+++ gulpfile.js
@@ -0,0 +1,212 @@
+'use strict';
+
+/**
+ * Import plugins
+ */
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    runSequence = require('run-sequence'),
+    argv = require('yargs').argv,
+    del = require('del');
+
+/**
+ * Build vendors dependencies
+ */
+gulp.task('vendors', function() {
+
+  /**
+   * CSS VENDORS
+   */
+  gulp.src([
+        ''
+      ])
+      .pipe($.concat('vendors.css'))
+      .pipe($.minifyCss())
+      .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/css'));
+
+  /**
+   * JS VENDORS
+   * (with jQuery and Bootstrap dependencies first)
+   */
+
+  gulp.src([
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/affix.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/button.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/carousel.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/collapse.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/modal.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tooltip.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/popover.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/scrollspy.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tab.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/transition.js'
+    ])
+    .pipe($.concat('vendors.min.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/js'));
+
+
+  /**
+   * FONTS SOURCES
+   * Important to add the bootstrap fonts to avoid issues with the fonts include path
+   */
+  gulp.src([
+      'bower_components/font-awesome/fonts/*',
+      'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
+      'drupal/sites/all/themes/antistatique/assets/fonts/*'
+    ])
+    .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/fonts'));
+
+  /**
+   * POLYFILLS SOURCES
+   * Various polyfills required for old IE
+   */
+  gulp.src([
+      'bower_components/html5shiv/dist/html5shiv.js',
+      'bower_components/respond/dest/respond.src.js'
+    ])
+    .pipe($.concat('polyfills.min.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/js'));
+});
+
+/**
+ * Copy images
+ */
+gulp.task('img', function() {
+  gulp.src([
+      'drupal/sites/all/themes/antistatique/assets/img/**/*'
+    ])
+    .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/img'));
+
+  gulp.src([
+      'drupal/sites/all/themes/antistatique/assets/svg/**/*'
+    ])
+    .pipe($.svgmin())
+    .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/img'));
+});
+
+/**
+ * Build styles from SCSS files
+ * With error reporting on compiling (so that there's no crash)
+ */
+gulp.task('styles', function() {
+  if (!argv.dev) { console.log('[styles] Processing minified styles.' ); }
+  else { console.log('[styles] Processing styles for dev env. No minifying here, for sourcemaps!') }
+
+  return gulp.src('drupal/sites/all/themes/antistatique/assets/sass/main.scss')
+    .pipe($.sass({errLogToConsole: true}))
+    .pipe($.if(argv.dev, $.sourcemaps.init()))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'ff 27', 'opera 12.1']
+    }))
+    .pipe($.if(argv.dev, $.sourcemaps.write()))
+    .pipe($.if(!argv.dev, $.minifyCss()))
+    .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/css'));
+});
+
+/**
+ * Build styles from SCSS files
+ * Only for STYLEGUIDE styles
+ */
+gulp.task('styleguide-styles', function() {
+  return gulp.src('drupal/sites/all/themes/antistatique/assets/sass/styleguide.scss')
+    .pipe($.sass({errLogToConsole: true}))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'ff 27', 'opera 12.1']
+    }))
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/css'));
+});
+
+/**
+ * Build JS
+ * With error reporting on compiling (so that there's no crash)
+ * And jshint check to highlight errors as we go.
+ */
+gulp.task('scripts', function() {
+  return gulp.src('drupal/sites/all/themes/antistatique/assets/js/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.concat('main.js'))
+    .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/js'))
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe($.uglify())
+    .pipe(gulp.dest('drupal/sites/all/themes/antistatique/build/js'));
+});
+
+/**
+ * Build Hologram Styleguide
+ */
+gulp.task('styleguide', function () {
+  return gulp.src('hologram_config.yml')
+    .pipe($.hologram());
+});
+
+
+/**
+ * Compile TWIG example pages
+ */
+
+gulp.task('twig', function () {
+    return gulp.src('drupal/sites/all/themes/antistatique/assets/pages/*.twig')
+        .pipe($.twig())
+        .pipe(gulp.dest('styleguide/pages'));
+});
+
+
+/**
+ * Clean output directories
+ */
+gulp.task('clean', del.bind(null, ['build', 'styleguide']));
+
+/**
+ * Serve
+ */
+gulp.task('serve', ['styles', 'scripts', 'twig'], function () {
+  browserSync({
+    server: {
+      baseDir: ['styleguide'],
+    },
+    open: false
+  });
+  gulp.watch(['drupal/sites/all/themes/antistatique/assets/sass/**/*.scss'], function() {
+    runSequence('styles', 'styleguide', reload);
+  });
+  gulp.watch(['drupal/sites/all/themes/antistatique/assets/sass/styleguide.scss'], function() {
+    runSequence('styleguide-styles', 'styleguide', reload);
+  });
+  gulp.watch(['drupal/sites/all/themes/antistatique/assets/img/**/*'], function() {
+    runSequence('img', 'styleguide', reload);
+  });
+  gulp.watch(['drupal/sites/all/themes/antistatique/assets/js/**/*.js'], function() {
+    runSequence('scripts', reload);
+  });
+
+  gulp.watch(['drupal/sites/all/themes/antistatique/assets/pages/**/*'], function() {
+    // clean folder before compiling
+    del.bind(null, ['styleguide/pages'])
+    runSequence('twig', reload);
+  });
+
+});
+
+/**
+ * Task to build assets on production server
+ */
+gulp.task('build',['clean'], function() {
+    runSequence('vendors', 'styles', 'img', 'scripts');
+});
+
+/**
+ * Default task
+ */
+gulp.task('default', ['clean'], function(cb) {
+  runSequence('vendors', 'styles', 'img', 'scripts','twig', 'styleguide-styles', 'styleguide', cb);
+});
+
