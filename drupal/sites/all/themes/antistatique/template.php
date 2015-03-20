a7795263dd44a7f033d157dfbf555a94f395645b
Toni Fisler <mail@tonifisler.ch>
2015-03-20 16:17:21 +0100

    remove broken function detecting if header is dark. TODO: apply this class to body

--- drupal/sites/all/themes/antistatique/template.php
+++ drupal/sites/all/themes/antistatique/template.php
@@ -5,11 +5,6 @@
  */
 
 function antistatique_preprocess_node(&$vars) {
-  // set title as white if image is dark
-  if ($vars['field_hero_image_is_dark'][0]['value'] == '1') {
-    $vars['title_attributes_array']['class'][] = 'text-white';
-  }
-
 }
 
 function antistatique_preprocess_field(&$vars) {
