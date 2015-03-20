d89afd9493da41dd6069e019cecbcfa83aed75e9
Toni Fisler <mail@tonifisler.ch>
2015-03-20 15:44:30 +0100

    fix navbar with new link-underline way

--- drupal/sites/all/themes/antistatique/templates/page/page.tpl.php
+++ drupal/sites/all/themes/antistatique/templates/page/page.tpl.php
@@ -106,20 +106,14 @@
         </button>
       </div>
 
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+      <?php if (!empty($primary_nav)): ?>
         <div class="navbar-collapse collapse">
           <nav role="navigation">
             <?php if (!empty($primary_nav)): ?>
               <?php print render($primary_nav); ?>
             <?php endif; ?>
-            <?php if (!empty($secondary_nav)): ?>
-              <?php print render($secondary_nav); ?>
-            <?php endif; ?>
-            <?php if (!empty($page['navigation'])): ?>
-              <?php print render($page['navigation']); ?>
-            <?php endif; ?>
           </nav>
-          <div class="nav navbar-right">
+          <div class="nav navbar-right navbar-btn">
             <a href="/contact" class="btn btn-primary"><?php print t('Contact Us'); ?></a>
           </div>
         </div>
