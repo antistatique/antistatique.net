01fd78d5b15af0a0ee82ec5352d44e3ba439b230
Toni Fisler <mail@tonifisler.ch>
2015-03-27 11:10:10 +0100

    use bundler with hologram

--- gulpfile.js
+++ gulpfile.js
@@ -209,7 +209,7 @@ gulp.task('scripts', function() {
  */
 gulp.task('styleguide', function () {
   return gulp.src('hologram_config.yml')
-    .pipe($.hologram());
+    .pipe($.hologram({bundler:true}));
 });
 
 /**
