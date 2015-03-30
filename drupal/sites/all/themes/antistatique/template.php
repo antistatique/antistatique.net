f5d1fe334f593ba52377fedaac92cc97c5518384
Toni Fisler <mail@tonifisler.ch>
2015-03-30 11:14:41 +0200

    update media module

--- drupal/sites/all/themes/antistatique/template.php
+++ drupal/sites/all/themes/antistatique/template.php
@@ -5,6 +5,16 @@
  */
 
 function antistatique_preprocess_node(&$vars) {
+  // Get a list of all the regions for this theme
+  foreach (system_region_list($GLOBALS['theme']) as $region_key => $region_name) {
+    // Get the content for each region and add it to the $region variable
+    if ($blocks = block_get_blocks_by_region($region_key)) {
+      $variables['region'][$region_key] = $blocks;
+    }
+    else {
+      $variables['region'][$region_key] = array();
+    }
+  }
 }
 
 function antistatique_preprocess_field(&$vars) {
