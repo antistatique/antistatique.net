a7ec49c536fef94269306eba9dba38512cd8e6ca
Toni Fisler <mail@tonifisler.ch>
2015-04-09 11:03:06 +0200

    update picture module

--- drupal/sites/all/modules/picture/picture.js
+++ drupal/sites/all/modules/picture/picture.js
@@ -1,5 +1,5 @@
 if (typeof Drupal !== 'undefined' && typeof jQuery !== 'undefined') {
-  // only load if Drupal and jQuery are defined.
+  // Only load if Drupal and jQuery are defined.
   (function ($) {
     Drupal.behaviors.picture = {
       attach: function (context) {
