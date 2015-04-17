262f3aef0a9f99430d8b2ebdcd1d6e63dd471d25
Toni Fisler <mail@tonifisler.ch>
2015-04-17 10:49:24 +0200

    add bg-foggy to title of news blocks too

--- drupal/sites/all/themes/antistatique/templates/block/block.tpl.php
+++ drupal/sites/all/themes/antistatique/templates/block/block.tpl.php
@@ -44,6 +44,11 @@
  *
  * @ingroup themeable
  */
+
+if ( $block->delta== "news-block_foggy_other" ) {
+  $classes .= ' bg-foggy';
+}
+
 ?>
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
