2481e07846ceca1a28b3f017cfdd434b75e2b94d
Toni Fisler <mail@tonifisler.ch>
2015-05-05 13:30:37 +0200

    add hi message in console :joy:

--- drupal/sites/all/themes/antistatique/assets/js/main.js
+++ drupal/sites/all/themes/antistatique/assets/js/main.js
@@ -3,4 +3,8 @@
   $('.ctools-jump-menu-select').selectpicker({
     style: 'btn-wire'
   });
+
+  // say hi
+  console.log('%cAntistatique', 'font-size:100px; color: #EE257A; font-family: "Arial"; font-weight: 600;');
+
 })(jQuery);
