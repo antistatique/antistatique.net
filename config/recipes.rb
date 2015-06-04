7d2c4a5809434689d2052c7e695be79dbbdb40ad
Toni Fisler <mail@tonifisler.ch>
2015-06-04 17:22:15 +0200

    put node_modules and bower_components in shared folder on server

--- config/recipes.rb
+++ config/recipes.rb
@@ -10,8 +10,10 @@ end
 namespace :assets do
   desc "Build assets on the server"
   task :build do
-    run "cd #{current_release} && npm config set spin=false && npm install --silent"
-    run "cd #{current_release} && ./node_modules/.bin/bower install --config.interactive=false"
+    run "cp #{current_release}/bower.json #{shared_path}/bower.json && cp #{current_release}/package.json #{shared_path}/package.json"
+    run "cd #{shared_path} && npm config set spin=false && npm install --silent"
+    run "cd #{shared_path} && ./node_modules/.bin/bower install --config.interactive=false"
+    run "ln -s #{shared_path}/bower_components #{current_release}/bower_components && ln -s #{shared_path}/node_modules #{current_release}/node_modules"
     run "cd #{current_release} && ./node_modules/.bin/gulp build"
   end
 end
