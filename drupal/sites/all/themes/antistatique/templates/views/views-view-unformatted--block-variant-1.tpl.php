cc3bcb4dbd70e37da56c41ab19ed1ab13adaa550
Toni Fisler <mail@tonifisler.ch>
2015-06-08 16:39:18 +0200

    small tweak to projects columns

--- drupal/sites/all/themes/antistatique/templates/views/views-view-unformatted--block-variant-1.tpl.php
+++ drupal/sites/all/themes/antistatique/templates/views/views-view-unformatted--block-variant-1.tpl.php
@@ -13,7 +13,7 @@
 <div class="container">
   <div class="row">
     <?php foreach ($rows as $id => $row): ?>
-      <?php $col = $id > 1 ? 'col-sm-4' : 'col-sm-6'; ?>
+      <?php $col = $id > 1 ? 'col-lg-4 col-sm-6' : 'col-sm-6'; ?>
       <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .' ' . $col . '"';  } ?>>
         <?php print $row; ?>
       </div>
