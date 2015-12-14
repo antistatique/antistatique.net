0cbe34ce7d81656ea15fd36fdb4b9ce9ec6ff707
Toni Fisler <mail@tonifisler.ch>
2015-12-14 14:58:57 +0100

    fix .htaccess and add X-UA-Compatible meta tag

--- drupal/sites/all/themes/antistatique/templates/html.tpl.php
+++ drupal/sites/all/themes/antistatique/templates/html.tpl.php
@@ -48,6 +48,7 @@
 <head profile="<?php print $grddl_profile; ?>">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
