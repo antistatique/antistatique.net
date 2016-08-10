c309413e94bd685db3e758fdec8732d8746a7f0f
Kevin Wenger <kevin@antistatique.net>
2016-08-10 19:23:54 +0200

    Avoid intercom to allow message when we are closed

--- drupal/sites/all/themes/antistatique/templates/html.tpl.php
+++ drupal/sites/all/themes/antistatique/templates/html.tpl.php
@@ -112,12 +112,15 @@ global $base_path;
   ?>
 
   <!-- intercom -->
-  <script>
-    window.intercomSettings = {
-      app_id: "l40asf6j"
-    };
-  </script>
-  <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/l40asf6j';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+  <!-- Avoid intercom to allow message when we are closed -->
+  <?php if ( (date('N') < 6) && date('H') > 8 && date('H') < 18 ): ?>
+      <script>
+        window.intercomSettings = {
+          app_id: "l40asf6j"
+        };
+      </script>
+      <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/l40asf6j';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+  <?php endif; ?>
 
   <script type="text/javascript">
     (function () {
