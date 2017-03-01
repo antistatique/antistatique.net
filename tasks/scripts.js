efa15115f65f5000916df9a91c04905692231f46
Toni Fisler <mail@tonifisler.ch>
2017-03-01 15:33:14 +0100

    fix breadcrumb menu

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,7 +1,5 @@
 import gulp from 'gulp';
-import webpack from 'webpack';
 import config from '../gulp_config.json';
-import webpackSettings from '../webpack.prod.config';
 import yargs from 'yargs';
 
 import loadPlugins from 'gulp-load-plugins';
@@ -20,26 +18,17 @@ function errorAlert(error){
  * With error reporting on compiling (so that there's no crash)
  * And jshint check to highlight errors as we go.
  */
-export const scriptsBuild = (done) => {
-  // run webpack
-  if (yargs.argv.production || yargs.argv.ghpages) {
-    webpack(webpackSettings, function(err, stats) {
-      if(err) throw new $.util.PluginError('webpack', err);
-      $.util.log('[webpack]', stats.toString({
-        cached: false,
-        colors: true,
-      }));
-      done();
-    });
-  } else { done(); }
-};
+gulp.task('scripts', function() {
 
-export const scriptsLint = () => {
-  return gulp.src(`${config.assets}js/index.js`)
+});
+export const scriptsBuild = () => {
+  return gulp.src(config.assets + 'js/*.js')
     .pipe($.plumber({errorHandler: errorAlert}))
-    .pipe($.eslint())
-    .pipe($.eslint.format());
+    .pipe($.concat('main.js'))
+    .pipe($.if(yargs.argv.production, $.uglify()))
+    .pipe($.size({title: 'JS SCRIPTS', showFiles: true}))
+    .pipe(gulp.dest(config.build + 'js'));
 };
 
-export const scripts = gulp.series(scriptsLint, scriptsBuild);
+export const scripts = gulp.series(scriptsBuild);
 export const scriptsTask = gulp.task('scripts', scripts);
